672cd8adbdd5f6a27fed17b6fa63e24c1cfc0258
thaidat186 <thaidat186@gmail.com>
2022-04-06 21:33:44 +0700

    bai thi

---
+++ app.js
@@ -0,0 +1,48 @@
+// A1
+// let ten = ["BINH", "HUNG", "PHUOC", "CAO", "KHANH"];
+// function maxLength(ten){
+//     let z = [ten[0]] ;
+//     let a = ten[0].length;
+//     for (let i = 1;i < ten.length; i++){
+//         if(ten[i].length == a){
+//             z.push(ten[i])
+//         }
+//         else if (ten[i].length > a){
+//             z = [ten[i]];
+//             a = ten[i].length;
+//         }
+//     }
+//     return z
+// }
+
+// console.log(maxLength(ten))
+
+
+//A2
+
+// let number = [60, 40, 55, 75, 64];
+// function choosePlayer(number){
+//     let c = [0, 0];
+    
+//     for (let i = 0 ; i < number.length; i++){
+//         if(i % 2 == 0 ){
+//             c[0]     += number[i]
+//         }       
+//         else {
+//             c[1] += number[i]
+//         }
+    
+//     }
+
+//     return c
+// }
+
+// console.log(choosePlayer(number));
+
+
+// B
+
+let buttonChange = document.getElementsByClassName('btn')
+buttonChange.onclick = function (){
+    
+}
